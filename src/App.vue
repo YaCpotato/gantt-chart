05ce539e58bd5ecaf06556063783c3532d3433c9
YaCpotato <yasshisshy@gmail.com>
2021-04-01 19:46:16 +0900

    tailwindu動作確認OK

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
     <div class="gantt-area">
       <svg id="gantt"></svg>
     </div>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div class="flex-shrink-0">
+        <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +20,6 @@
 import Gantt from 'frappe-gantt'
 import './store'
 import Task from './model/Task'
-
 export default {
   name: 'App',
   data () {
@@ -64,7 +72,13 @@ export default {
     methods: {
       initialize () {
         this.task = Task.query().where('project_id',this.project.id).get()
-      }
+      },
+      show () {
+      this.$modal.show('hello-world');
+      },
+      hide () {
+        this.$modal.hide('hello-world');
+      },
     }
 }
 </script>
