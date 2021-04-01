a5cf1d6007df70c631b7d75aa1b54cc6e440e55f
YaCpotato <yasshisshy@gmail.com>
2021-04-01 19:22:07 +0900

    frappe追加機能実装

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,71 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <svg id="gantt"></svg>
+    <div class="gantt-area">
+      <svg id="gantt"></svg>
+    </div>
   </div>
 </template>
 
 <script>
-import Gantt from "frappe-gantt" 
+import Gantt from 'frappe-gantt'
+import './store'
+import Task from './model/Task'
 
 export default {
   name: 'App',
   data () {
     return {
+      project: {
+        'id': null,
+        'name': '',
+        'day': ''
+      },
       tasks:[
         {
-	id: 'Task 1',
-	name: 'Redesign website',
-	start: '2016-12-28',
-	end: '2016-12-31',
-	progress: 20,
-	dependencies: 'Task 2, Task 3'
-  }
+          id: 'Task 1',
+          name: 'Redesign website',
+          start: '2016-12-28',
+          end: '2016-12-31',
+          progress: 20,
+          dependencies: 'Task 2, Task 3',
+          summary:"aaaaaaa"
+        }
       ],
       gantt: null
     }
   },
   mounted () {
-    this.gantt = new Gantt("#gantt", this.tasks);
-  }
+    this.gantt = new Gantt("#gantt", this.tasks,{
+      on_click (task) {
+        console.log(task)
+      },
+      on_date_change (task, start, end) {
+        console.log(task, start, end);
+      },
+      on_progress_change (task, progress) {
+        console.log(task, progress);
+      },
+      on_view_change (mode) {
+        console.log(mode);
+      },
+      custom_popup_html (task) {
+        return `
+        <div class="details-container">
+          <h5>${task.name}</h5>
+          <p>Expected to finish by ${task.end}</p>
+          <p>${task.summary}</p>
+        </div>
+        `;
+      }
+    }
+    );
+  },
+    methods: {
+      initialize () {
+        this.task = Task.query().where('project_id',this.project.id).get()
+      }
+    }
 }
 </script>
 
@@ -39,5 +77,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.gantt-area {
+  max-width: 80vw;
+  overflow: scroll;
+  text-align: center;
 }
 </style>
