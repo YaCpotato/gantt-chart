754be1f8d8737706e7beeeb0edcbd0bac11025ed
YaCpotato <yasshisshy@gmail.com>
2021-04-01 20:15:51 +0900

    docs追加

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-// module.exports = {
-//     indexPath: './docs/index.html',
-//     publicPath: 'https://yacpotato.github.io/',
-//     assetsDir: './assets/'
-//   }
+module.exports = {
+    outputDir: 'docs'
+  }
